dd446c3f247b6f219b071248e01b5de04e8cf9a6
柏葉　祐輝 <yuki.kashiwaba@gmail.com>
2021-03-29 16:38:09 +0900

    Sarsaのソースコードを実装

---
+++ Sarsa/main.py
@@ -0,0 +1,31 @@
+import os
+import gym
+import datetime
+from frozen_lake_util import show_q_value
+from agent import Agent
+from logger import Logger
+from trainer import Trainer
+
+def main():
+    # ログ出力先の設定
+    now = datetime.datetime.now()
+    dst_path = os.path.join("log/", now.strftime('%Y%m%d_%H%M%S'))
+
+    # 環境、エージェント、ロガー、トレーナーの初期化
+    obs = gym.make("FrozenLakeEasy-v0")
+    agent = Agent()
+    logger = Logger(os.path.join(dst_path, "log.txt"))
+    trainer = Trainer()
+
+    # 学習実行
+    trainer.train(agent, obs, logger)
+
+    # ログ出力
+    logger.plot(key="reward", freq=50, save_path=os.path.join(dst_path, "reward.png"))
+
+    # 学習したエージェントで1エピソード実行
+    trainer.play(agent, obs)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Sarsa/trainer.py
@@ -0,0 +1,60 @@
+class Trainer():
+    def __init__(self):
+        pass
+    
+    # 学習
+    def train(self, agent, env, logger, episode_count=10000, gamma=0.9, learning_rate=0.1, render=False, disp_freq=100):
+        # エージェントの初期化
+        actions = list(range(env.action_space.n))
+        agent.initialize(actions)
+
+        # ロガーの初期化
+        config_data = {
+            "episode_count": episode_count,
+            "gamma": gamma,
+            "learning_rate": learning_rate
+        }
+        logger.initialize(str(env.spec), config_data)
+
+        # 指定した回数分エピソードを実行
+        for e in range(episode_count):            
+            s = env.reset()
+            a = agent.policy(s)
+            episode_reward = 0.0
+            done = False
+            # エピソード実行
+            while not done:
+                if render:
+                    env.render()
+                
+                # エージェントの方策にしたがって環境を更新
+                n_state, reward, done, info = env.step(a)
+
+                # 現在の見積もり値（estimated）と実測した値（gain）からTD誤差を計算
+                n_a = agent.policy(n_state)
+                gain = reward + gamma * agent.Q[n_state][n_a]
+                estimated = agent.Q[s][a]
+                agent.Q[s][a] += learning_rate * (gain - estimated)     # Q値を更新
+
+                # 状態を更新
+                s = n_state
+                a = n_a
+                episode_reward += reward
+        
+            logger.add({"episode":e, "reward":episode_reward})
+            # 一定間隔ごとにlog表示
+            if e % disp_freq == 0:
+                print("At episode {}, reward={}".format(e, logger.get("reward", disp_freq)))
+
+    
+    # 学習結果を使って1エピソード実行
+    def play(self, agent, env):
+        agent.eval()
+        s = env.reset()
+        done = False
+        while not done:
+            env.render()
+            a = agent.policy(s)
+            n_state, reward, done, info = env.step(a)
+            s = n_state
+        env.render()
